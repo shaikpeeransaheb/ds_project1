23660eb15b90156875ddd19fb748262fbbf3ad16
shaikpeeransaheb <peransahebs@gmail.com>
2026-09-04 23:23:11 +0530

    Add function to reverse linked list
    Implement function to reverse linked list data.

---
+++ reversedata.c
@@ -0,0 +1,25 @@
+#include"header.h"
+void reversedata(void)
+{
+        if(head==0)
+        {
+                printf("no records found:\n");
+                return ;
+        }
+        int i;
+        int c=countnode();
+        struct stu **p,*t=head;
+        if(c>1)
+        {
+                p=malloc(sizeof(struct stu*)*c);
+                for(i=0;i<c;i++)
+                {
+                        p[i]=t;
+                        t=t->next;
+                }
+                for(i=c-1;i>0;i--)
+                        p[i]->next=p[i-1];
+                        p[0]->next=0;
+                        head=p[c-1];
+        }
+}
